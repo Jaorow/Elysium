f740a61973064c005ca4ead04059ae8eaf17fd14
Jamie <92243661+Jaorow@users.noreply.github.com>
2023-07-15 10:41:51 +1200

    update to allow use of CORS

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -26,6 +26,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 app.UseAuthorization();
 
 app.MapControllers();
