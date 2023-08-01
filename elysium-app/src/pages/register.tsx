b256ceffaa44673b26a0b89d0061cc810538b79e
Jamie <92243661+Jaorow@users.noreply.github.com>
2023-08-01 20:53:15 +1200

    register and login stylised

--- elysium-app/src/pages/register.tsx
+++ elysium-app/src/pages/register.tsx
@@ -1,38 +1,101 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import '../App.css';
 
 
 const Register: React.FC = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+      });
+    
+      const handleInputChange = (
+        event: React.ChangeEvent<HTMLInputElement>,
+      ) => {
+        const { name, value } = event.target;
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          [name]: value,
+        }));
+      };
+    
+      const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        // You can perform registration logic here using the formData
+        console.log('Registration form submitted:', formData);
+        // Reset the form after successful registration
+        setFormData({
+          username: '',
+          password: '',
+        });
+      };
   return (
     <div className="page">
-      <div className="form">
-          <div className="form-body">
-              <div className="username">
-                  <label className="form__label" htmlFor="firstName">First Name </label>
-                  <input className="form__input" type="text" id="firstName" placeholder="First Name"/>
-              </div>
-              <div className="lastName">
-                  <label className="form__label" htmlFor="lastName">Last Name </label>
-                  <input  type="text" name="" id="lastName"  className="form__input"placeholder="LastName"/>
-              </div>
-              <div className="email">
-                  <label className="form__label" htmlFor="email">Email </label>
-                  <input  type="email" id="email" className="form__input" placeholder="Email"/>
-              </div>
-              <div className="password">
-                  <label className="form__label" htmlFor="password">Password </label>
-                  <input className="form__input" type="password"  id="password" placeholder="Password"/>
-              </div>
-              <div className="confirm-password">
-                  <label className="form__label" htmlFor="confirmPassword">Confirm Password </label>
-                  <input className="form__input" type="password" id="confirmPassword" placeholder="Confirm Password"/>
-              </div>
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign up
+          </h2>
+        </div>
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="username" className="sr-only">
+                Username
+              </label>
+              <input
+                id="username"
+                name="username"
+                type="text"
+                autoComplete="username"
+                required
+                value={formData.username}
+                onChange={handleInputChange}
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Username"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                value={formData.password}
+                onChange={handleInputChange}
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+              />
+            </div>
           </div>
-          <div className="footer">
-              <button type="submit" className="btn">Register</button>
+
+          <div className="flex items-center justify-center">
+            <div className="text-sm">
+              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                Already have an account? Sign in
+              </a>
+            </div>
           </div>
-      </div>      
+
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                {/* Add an icon here if you want */}
+              </span>
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
     </div>
   );
 };
